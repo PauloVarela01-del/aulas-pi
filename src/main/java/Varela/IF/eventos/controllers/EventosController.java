547839e1bf5f7a50586269fa57e2012088d1fe7b
Paulo Varela <paulo.varela@escolar.ifrn.edu.br>
2024-11-06 08:29:25 -0300

    Atualizando layout da nossa Tabela 1/2

--- src/main/java/Varela/IF/eventos/controllers/EventosController.java
+++ src/main/java/Varela/IF/eventos/controllers/EventosController.java
@@ -1,20 +1,26 @@
 package Varela.IF.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ModelAndViewMethodReturnValueHandler;
 
 import Varela.IF.eventos.models.Evento;
 import Varela.IF.eventos.repository.EventoRepositoy;
 
 @Controller
+@RequestMapping("/eventos")
 public class EventosController {
 
 	@Autowired
 	private EventoRepositoy er;
 	
-	@RequestMapping("/eventos/form")
+	@GetMapping("/form")
 	public String form() {
 		System.out.println("Chame o formulário");
 		
@@ -30,5 +36,13 @@ public class EventosController {
 		
 			return "eventos/evento-adicionado";
 		}
+	
+	@GetMapping
+	public ModelAndView Listar() {
+		List<Evento> eventos = er.findAll();
+		ModelAndView mv = new ModelAndView("Eventos/lista");
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
 }
 
