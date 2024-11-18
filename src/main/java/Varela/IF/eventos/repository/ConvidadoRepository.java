43f6c53a436e40908ed615a55f1838046d612995
Paulo Varela <paulo.varela@escolar.ifrn.edu.br>
2024-11-18 09:49:47 -0300

    apagando um evento da aplicação

--- src/main/java/Varela/IF/eventos/repository/ConvidadoRepository.java
+++ src/main/java/Varela/IF/eventos/repository/ConvidadoRepository.java
@@ -1,9 +1,15 @@
 package Varela.IF.eventos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import Varela.IF.eventos.models.Convidado;
+import Varela.IF.eventos.models.Evento;
 
 public interface ConvidadoRepository extends JpaRepository<Convidado, Long> {
+	
+ List<Convidado> findByEvento(Evento evento);
+ 
 
 }
